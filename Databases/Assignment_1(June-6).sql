e67b65b7af7e8a6f9eb767ede6e1be98409d05c6
ubiquitous-aman <amankumaraman112@gmail.com>
2026-06-10 22:38:52 +0530

    Completing the remaining questions

--- Databases/Assignment_1(June-6).sql
+++ Databases/Assignment_1(June-6).sql
@@ -74,8 +74,37 @@ GROUP BY
     r.region_name;
 
 -- How many days on average are customers reallocated to a different node ?
-
+SELECT AVG(
+        DATEDIFF(DAY, start_date, end_date)
+    ) AS avg_days
+FROM customer_nodes
+WHERE
+    end_date <> '9999-12-31';
 -- What is the median, 80th and 95th percentile for this same reallocation days metric for each region ?
+SELECT
+    r.region_name,
+    PERCENTILE_CONT(0.5) WITHIN GROUP ( --median
+        ORDER BY DATEDIFF(DAY, start_date, end_date)
+    ) OVER (
+        PARTITION BY
+            r.region_id
+    ) AS median_days,
+    PERCENTILE_CONT(0.8) WITHIN GROUP ( --80th percentile
+        ORDER BY DATEDIFF(DAY, start_date, end_date)
+    ) OVER (
+        PARTITION BY
+            r.region_id
+    ) AS percentile_80,
+    PERCENTILE_CONT(0.95) WITHIN GROUP ( --95th percentile
+        ORDER BY DATEDIFF(DAY, start_date, end_date)
+    ) OVER (
+        PARTITION BY
+            r.region_id
+    ) AS percentile_95
+FROM customer_nodes cn
+    JOIN regions r ON cn.region_id = r.region_id
+WHERE
+    end_date <> '9999-12-31';
 
 -----------------------------------------B. Customer Transaction----------------------------------------------
 -- What is the unique count and total amount for each transaction type?
@@ -101,7 +130,95 @@ FROM (
     ) t;
 
 -- For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
-
+WITH
+    monthly_summary AS (
+        SELECT
+            customer_id,
+            YEAR(txn_date) AS year_no,
+            MONTH(txn_date) AS month_no,
+            COUNT(
+                CASE
+                    WHEN txn_type = 'deposit' THEN 1
+                END
+            ) AS deposits,
+            COUNT(
+                CASE
+                    WHEN txn_type = 'purchase' THEN 1
+                END
+            ) AS purchases,
+            COUNT(
+                CASE
+                    WHEN txn_type = 'withdrawal' THEN 1
+                END
+            ) AS withdrawals
+        FROM customer_transactions
+        GROUP BY
+            customer_id,
+            YEAR(txn_date),
+            MONTH(txn_date)
+    )
+SELECT
+    year_no,
+    month_no,
+    COUNT(*) AS customer_count
+FROM monthly_summary
+WHERE
+    deposits > 1
+    AND (
+        purchases >= 1
+        OR withdrawals >= 1
+    )
+GROUP BY
+    year_no,
+    month_no
+ORDER BY year_no, month_no;
 -- What is the closing balance for each customer at the end of the month?
-
+SELECT
+    customer_id,
+    MONTH(txn_date) AS month_number,
+    SUM(
+        CASE
+            WHEN txn_type = 'deposit' THEN txn_amount
+            ELSE - txn_amount
+        END
+    ) AS closing_balance
+FROM customer_transactions
+GROUP BY
+    customer_id,
+    MONTH(txn_date)
+ORDER BY customer_id, month_number;
 -- What is the percentage of customers who increase their closing balance by more than 5%?
+WITH
+    monthly_balance AS (
+        SELECT
+            customer_id,
+            MONTH(txn_date) AS month_number,
+            SUM(
+                CASE
+                    WHEN txn_type = 'deposit' THEN txn_amount
+                    ELSE - txn_amount
+                END
+            ) AS balance
+        FROM customer_transactions
+        GROUP BY
+            customer_id,
+            MONTH(txn_date)
+    )
+SELECT ROUND(
+        COUNT(DISTINCT customer_id) * 100.0 / (
+            SELECT COUNT(DISTINCT customer_id)
+            FROM customer_transactions
+        ), 2
+    ) AS percentage_customers
+FROM (
+        SELECT
+            customer_id, month_number, balance, LAG(balance) OVER (
+                PARTITION BY
+                    customer_id
+                ORDER BY month_number
+            ) AS previous_balance
+        FROM monthly_balance
+    ) t
+WHERE
+    previous_balance IS NOT NULL
+    AND balance > previous_balance * 1.05;
